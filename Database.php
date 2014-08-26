87998a598c82bd4f395e0011c1fa375009409343
Niko <niksancho96@gmail.com>
2014-08-26 22:20:11 +0300

    Some new stuff
    Added some comments and a new function

--- Database.php
+++ Database.php
@@ -7,7 +7,11 @@
         // This variable stores the MySQLi connection data
         private $_connData;
         
-        // Establish a connection with the server
+        /*
+            * Establish connection with the server
+            * Example usage: $class = new Database();
+            * Return values: Only return value is, when the inital connection hits an error
+        */
         public function __construct() {
             $conn = new MySQLi('localhost', 'root', '', 'test');
             if ($conn->connect_errno) {
@@ -18,18 +22,30 @@
             }
         }
         
-        // Returns status of connection
+        /*
+            * Returns the current status of the connection
+            * Example usage: echo $class->getConnectionStatus();
+            * Return values: 'True' -> If connection is OK or 'False' -> If connection has a problem
+        */
         public function getConnectionStatus() {
             return $this->_connStatus;
         }
         
-        // Returns the object of the MySQLi connection
+        /*
+            * Returns the current connection data
+            * Example usage (For custom queries): $statement = mysqli_query($class->getConnectionData(), "Query goes here...");
+            * Return values: All data you can get from the Appache server
+        */
         public function getConnectionData() {
             return $this->_connData;
         }
 
-        // Fetches all data from a table, and returns the searching value as an array element
-        public function getAllDataFromTable($table, $searchData) {
+        /*
+            * Returns a specified data from a specified table
+            * Example usage: $class->getDataFromTable('Users', 'id');
+            * Return values: Returns a string with all id's from the 'Users' table
+        */
+        public function getDataFromTable($table, $searchData) {
             $statement = $this->_connData->query("SELECT * FROM " . $table . "");
             $count = $statement->num_rows;
 
@@ -40,5 +56,17 @@
                     echo $result[$searchData] . "\r\n";
                 }
             }
+        }
+
+        /*
+            * Inserts certain data to a table specified by the user
+            * $table - specify the table in which you want to insert data 
+            * $fields - string in which the user types the columns which he wants to affect in Database
+            * $data - string in which the user types the data that he wants to insert into the Database
+            * Example usage: $class->insertDataIntoTable('users', 'id, username, password, email', '$id, $username, $email');
+            * Return values: 'True' -> If query was OK or 'False' -> If query had a problem
+        */
+        public function insertDataIntoTable($table, $fields, $data) {
+            
         }
     }
